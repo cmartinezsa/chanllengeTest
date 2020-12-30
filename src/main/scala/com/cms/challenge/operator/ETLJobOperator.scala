df562cf506c77a2a830e961cfb2dd60687681094
MC <40157074+cmartinezsa@users.noreply.github.com>
2020-12-30 00:12:14 -0600

    Modificacion

--- src/main/scala/com/cms/challenge/operator/ETLJobOperator.scala
+++ src/main/scala/com/cms/challenge/operator/ETLJobOperator.scala
@@ -1,7 +1,6 @@
 package com.cms.challenge.operator
 
 import com.cms.challenge.common.Atributos
-import com.cms.challenge.etl.EtlTableCargo
 import com.cms.challenge.etl.StockPrices
 import org.apache.log4j.{LogManager, Logger}
 import org.apache.spark.sql.SparkSession
@@ -10,21 +9,18 @@ import com.cms.challenge.common.Args
 
 object ETLJobOperator extends Atributos {
   private val LOGGER: Logger = LogManager.getLogger(msjLog)
-  val ETLCARGO = new EtlTableCargo()
   val STOCK_PRICES = new StockPrices()
-
 
   def main(args: Array[String]): Unit = {
     println("INICIANDO PROCESO")
-    val SPARK = initializeSparkSession(args)
-    println("INICIANDO SPARK")
-    if (Args.checkParamsBoolean.equals(false)) {
+    val SPARK_SESSION = initializeSparkSession(args)
+    if (Args.checkParams.equals(false)) {
       LOGGER.info("FINALIZA EJECUCION ANTICIPADA")
-      SPARK.stop()
+      SPARK_SESSION.stop()
     } else {
       LOGGER.info("VALIDACION DE PAMETROS EXITOSO")
-      STOCK_PRICES.stockPricesETL(Args.pathFile, Args.datePart, Args.tickerList, Args.numDay)(SPARK)
-      SPARK.stop()
+      STOCK_PRICES.stockPricesETL(Args.pathFile, Args.datePart, Args.tickerList, Args.numDay)(SPARK_SESSION)
+      SPARK_SESSION.stop()
     }
     LOGGER.info("*** FINISHED ****")
   }
@@ -32,16 +28,12 @@ object ETLJobOperator extends Atributos {
   /**
     *
     * @param args
-    * @return
+    * @return SparkSession for orchestrate the process.
     */
   def initializeSparkSession(args: Array[String]): SparkSession = {
-    LOGGER.info("Iniciando Proceso")
-    println("INICIANDO INITIALIZED")
+    LOGGER.info("Initializing SparkSession")
     Args.getArguments(args)
-    println("initialized Mode : " + Args.modeExecution)
-    println("Initialized " + Args.processName)
-
-    if (Args.checkParamsBoolean.equals(true)) {
+    if (Args.checkParams.equals(true)) {
       SparkSession
         .builder()
         .config("spark.service.user.postgresql.pass", "cmsdb")
